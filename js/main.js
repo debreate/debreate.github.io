206c834f4a0a8d55f7280dc1e7211387ea7b43ae
Huluti <hugo.posnic@gmail.com>
2016-11-01 17:03:44 +0100

    Reduce need of JS by using pure CSS for popups

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('a[href*="#"]:not([href="#"])').click(function() {
+    $('a[href*="#"]:not([href="#"]):not([href="#release-notes"])').click(function() {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -38,30 +38,4 @@ $(document).ready(function() {
         $(this).next('#faqs dd').slideToggle(500);
         $(this).toggleClass('expanded');
     });
-
-    var appendthis = '<div class="modal-overlay js-modal-close"></div>';
-
-    $('a[data-modal-id]').click(function(e) {
-        e.preventDefault();
-        $('body').append(appendthis);
-        $('.modal-overlay').fadeTo(500, 0.7);
-        var modalBox = $(this).attr('data-modal-id');
-        $('#' + modalBox).fadeIn($(this).data());
-    });
-
-
-    $('.js-modal-close, .modal-overlay').click(function() {
-        $('.modal-box, .modal-overlay').fadeOut(500, function() {
-            $('.modal-overlay').remove();
-        });
-    });
-
-    $(window).resize(function() {
-        $('.modal-box').css({
-            top: ($(window).height() - $('.modal-box').outerHeight()) / 2,
-            left: ($(window).width() - $('.modal-box').outerWidth()) / 2
-        });
-    });
-
-    $(window).resize();
 });
